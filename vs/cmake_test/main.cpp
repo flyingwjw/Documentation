9d56df593723c30aea41dfa5cb716c9df2c1708a
kevin <kevin-fly@outlook.com>
2018-09-28 13:37:04 +0800

    Update cmake_test and vcpkg_test

--- vs/cmake_test/main.cpp
+++ vs/cmake_test/main.cpp
@@ -1,9 +1,14 @@
 #include <sqlite3.h>
+#include <zlib.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include <stdio.h>
 
 int main()
 {
-	printf("%s\n", sqlite3_libversion());
+	printf("sqlite3: %s\n", sqlite3_libversion());
+	printf("zlib: %s\n", zlibVersion());
 	getchar();
 	return 0;
 }
